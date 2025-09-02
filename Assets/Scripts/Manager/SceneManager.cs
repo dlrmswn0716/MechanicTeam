928a4b603541ec9afa80b7b3af2133066a0110fb
geunju <dlrmswn990716@gmail.com>
2025-09-02 20:16:02 +0900

    씬 전환 기본 로직 추가

--- Assets/Scripts/Manager/SceneManager.cs
+++ Assets/Scripts/Manager/SceneManager.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
     public static SceneManager instance;
-
+    [SerializeField] private SceneType _sceneType;    
+    
+   enum SceneType
+    {
+        Lobby,
+        Stage1,
+        Stage2,
+        Stage3
+    } 
     private void Awake()
     {
         if (instance == null)
@@ -13,6 +22,17 @@ public class SceneManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+    }
+
+    public void NextScene()
+    {
+        // 마지막 씬
+        int lastScene = System.Enum.GetValues(typeof(SceneType)).Cast<int>().Max();
+        _sceneType += 1;
+        if ((int)_sceneType > lastScene)
+            _sceneType = SceneType.Lobby;
+        string sceneName = _sceneType.ToString();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
