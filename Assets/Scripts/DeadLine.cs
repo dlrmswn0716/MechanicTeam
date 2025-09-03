7329030ae02fbb92cbecb5e974d6aa46cbc70d25
geunju <dlrmswn990716@gmail.com>
2025-09-03 18:11:14 +0900

    데드라인 추가

---
+++ Assets/Scripts/DeadLine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DeadLine : MonoBehaviour
+{
+
+    //떨어질 시 게임오버
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            UIManager.Instance.OverUI();
+        }
+    }
+}
