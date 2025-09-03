f40ce8f1e0d5d54649b6a6c77db709764f7349c8
Choi Jeongho <jeonghoe22@gmail.com>
2025-09-03 23:44:07 +0900

    로비에서 게임 종료 추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,6 +4,10 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
@@ -127,5 +131,14 @@ public class UIManager : MonoBehaviour
         MouseLock(false);
         timeOff = true;
         over.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
 }
